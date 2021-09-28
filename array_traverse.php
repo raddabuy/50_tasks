6d978b8443e2c66e3c39b422ff63efbccc0a1985
raddabuy <raddabuy@yandex.ru>
2021-09-28 13:56:50 +0500

    add array traverse

---
+++ array_traverse.php
@@ -0,0 +1,13 @@
+<?php
+
+$array = [[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]],[[13,14,15],[16,17,18]]];
+
+print_r(traverse($array));
+
+function traverse($array){
+    $result = [];
+    foreach ($array as $key => $value){
+        $result[$key] = $value;
+    }
+    return $result;
+}
