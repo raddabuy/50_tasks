764af59bba733761328f5c7f8483cc9ccc67b594
raddabuy <raddabuy@yandex.ru>
2021-09-28 14:16:16 +0500

    edit array traverse

--- array_traverse.php
+++ array_traverse.php
@@ -2,12 +2,16 @@
 
 $array = [[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]],[[13,14,15],[16,17,18]]];
 
-print_r(traverse($array));
+traverse($array);
 
 function traverse($array){
     $result = [];
     foreach ($array as $key => $value){
-        $result[$key] = $value;
+        if (is_array($value)) {
+            traverse($value);
+        } else {
+            echo $value."\n";
+        }
     }
     return $result;
 }
